2fabd823ac5817ead56a9bdaae9e11650ec2bc87
aria198a <aria198a@gmail.com>
2023-11-18 13:22:42 +0800

    景泰第二次新增

--- scripts/click.js
+++ scripts/click.js
@@ -1,5 +1,11 @@
 var followButton = document.getElementById("followButton");
 
 followButton.addEventListener("click", function() {
-    window.location.href = "index3.html";
+    window.location.href = "parkingDetail.html";
 });
+
+var canelButton = document.getElementById("cancelButton");
+
+canelButton.addEventListener("click", function() {
+    window.location.href = "findSpace.html";
+})
---
+++ scripts/click2.js
@@ -0,0 +1,11 @@
+var rightButton = document.querySelector(".rightButton");
+
+rightButton.addEventListener("click", function() {
+    window.location.href = "correct.html";
+})
+
+var canelButton = document.querySelector(".cancelButton");
+
+canelButton.addEventListener("click", function() {
+    window.location.href = "findSpace.html";
+})
