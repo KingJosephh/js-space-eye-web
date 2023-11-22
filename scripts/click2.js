78a4559affdcd459431dbbc750059c1ad4a48241
aria198a <aria198a@gmail.com>
2023-11-22 11:23:54 +0800

    預約停車功增

--- scripts/click2.js
+++ scripts/click2.js
@@ -1,10 +1,46 @@
-var rightButton = document.querySelector(".rightButton");
+var btn = document.getElementById("next");
+
+
+function select(el, left) {
+    var plan = "";
+    if (!plan) {
+        btn.disabled = false;
+    }
+
+    Array.from(el.parentElement.children).forEach(function (child) {
+        child.classList.remove("plan-active");
+    });
+
+    el.classList.add("plan-active");
+
+    if (left) {
+        el.parentElement.children[2].classList.remove("plan-active");
+        btn.innerHTML = "每週停車";
+    } else if (el.nextElementSibling) {
+        el.nextElementSibling.classList.remove("plan-active");
+        btn.innerHTML = "每月停車";
+    } else if (el.previousElementSibling) {
+        el.previousElementSibling.classList.remove("plan-active");
+        btn.innerHTML = "每年停車";
+    }
+}
+
+
+
+var rightButton = document.querySelector(".btn-primary");
 
 rightButton.addEventListener("click", function() {
-    window.location.href = "correct.html";
+    var entryDateValue = document.getElementById("entryDate").value;
+    var exitDateValue = document.getElementById("exitDate").value;
+    
+    if (!entryDateValue && !exitDateValue) {
+        alert ("請選擇入場日期和離場日期");
+    }else{
+      window.location.href = "correct.html";
+    }
 })
 
-var canelButton = document.querySelector(".cancelButton");
+var canelButton = document.querySelector(".btn-secondary");
 
 canelButton.addEventListener("click", function() {
     window.location.href = "findSpace.html";
---
+++ scripts/click3.js
@@ -0,0 +1,5 @@
+var paymentButton = document.querySelector(".btn-payment");
+
+paymentButton.addEventListener("click", function() {
+    window.location.href = "payment.html";
+})
