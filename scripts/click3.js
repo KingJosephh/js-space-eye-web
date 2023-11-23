fd86996f20523c83691319ecbe820ff08b31b1b9
aria198a <aria198a@gmail.com>
2023-11-23 19:55:36 +0800

    關閉按鈕設置by景泰

--- scripts/click3.js
+++ scripts/click3.js
@@ -3,3 +3,10 @@ var paymentButton = document.querySelector(".btn-payment");
 paymentButton.addEventListener("click", function() {
     window.location.href = "payment.html";
 })
+
+var closeBtn = document.getElementById("closeBtn");
+
+closeBtn.addEventListener("click", function() {
+    const watermark = document.querySelector(".watermark")
+    watermark.style.display = "none";
+})
