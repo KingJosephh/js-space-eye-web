09266075e4e4086ecd47005d53a67d429aa28519
Celine <celinewu1010@gmail.com>
2023-12-01 16:48:40 +0800

    header辨識登入，更改按鈕樣式與連結

--- stylesheets/components/header/header.js
+++ stylesheets/components/header/header.js
@@ -1,6 +1,32 @@
-const navbar = document.querySelector('.navbar');
-const hamburger = document.querySelector('.navbar-toggler');
-const navbarCollapse = document.querySelector('.navbar-collapse');
+// const navbar = document.querySelector('.navbar');
+// const hamburger = document.querySelector('.navbar-toggler');
+// const navbarCollapse = document.querySelector('.navbar-collapse');
+
+// 辨識是否已登入
+
+
+  const userBtn = document.querySelector('#userBtn');
+  // let Url = 'http://localhost:3000';
+  // const token = localStorage.getItem('token');
+  // const usersId = localStorage.getItem('usersId');
+  axios.get(Url + `/600/users/${usersId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      userBtn.children[0].innerText = "車主專區";
+      userBtn.children[0].setAttribute('href', '/Pages/carOwnerNew.html');
+    })
+    .catch((error) => {
+      console.log(error);
+      userBtn.children[0].innerText = "登入 / 註冊";
+      userBtn.children[0].setAttribute('href', '/Pages/login.html');
+    });
+
+
+
 // 監聽視窗變化移除黑底樣式
 /* window.addEventListener('resize', e => {
   let screenWidth = document.body.offsetWidth;
